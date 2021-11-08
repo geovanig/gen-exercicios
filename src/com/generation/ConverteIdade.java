d4c4eef401c7a30ef675aef0cbe9a21ca2410205
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-11-08 14:25:31 -0300

    Pasta com exercicios em Java

---
+++ src/com/generation/ConverteIdade.java
@@ -0,0 +1,30 @@
+package com.generation;
+//1. Faça um sistema que leia a idade de uma pessoa expressa em anos,
+//meses e dias e mostre-a expressa apenas em dias. 
+
+import java.util.Scanner;
+
+public class ConverteIdade {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Coloque, aqui, quantos anos você tem: ");
+		int anos = entrada.nextInt();
+		
+		System.out.println("Coloque, aqui, quantos meses você possui, depois do seu aniversário: ");
+		int meses = entrada.nextInt();
+		
+		System.out.println("Coloque, aqui, quantos dias você tem, depois da quantidade de meses completos: ");
+		int dias = entrada.nextInt();
+		
+		int diasVividos = (anos*365) + (meses*30) + dias;
+		
+		System.out.println("Você viveu o total de " + diasVividos + " dias28");
+		
+		entrada.close();
+
+	}
+
+}
