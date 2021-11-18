de23d90d55a730185558b4980022077f40935794
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-11-17 21:56:30 -0300

    Resto do exercicios de poo

--- src/poo/heranca/Cavalo.java
+++ src/poo/heranca/Cavalo.java
@@ -1,5 +1,33 @@
 package poo.heranca;
 
 public class Cavalo extends Animal {
+	
+	private String nome;
+	private int idade;
+	
+	public Cavalo(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	@Override
+	void emitirSom() {
+		System.out.println("relinchando...");
+	}
 
 }
